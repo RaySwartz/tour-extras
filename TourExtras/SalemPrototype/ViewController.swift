93e8888354a8e49713da75e088eba4274150b0a2
Ray Swartz <rayswartz@mac.com>
2018-12-18 16:10:30 -0500

    Created target called SalemPrototype. Added some stub images.

---
+++ TourExtras/SalemPrototype/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SalemPrototype
+//
+//  Created by Raymond Edgar Swartz on 12/18/18.
+//  Copyright © 2018 Swartzware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let initialImage = 1
+    }
+
+
+}
+
